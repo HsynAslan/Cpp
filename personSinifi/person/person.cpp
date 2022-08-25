f253ac3d9c5fa9089b9b5699e24a1157e94b91e0
Hüseyin Aslan <91592183+HsynAslan@users.noreply.github.com>
2022-08-25 13:55:39 +0300

    Add files via upload

---
+++ personSinifi/person/person.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+#include "person.h"
+int main()
+{
+    using namespace std;
+
+    Person* pArray[] = { new TurkishPerson("Huseyin"),new TurkishPerson("Baris"),
+    new TurkishPerson("Dogukan"),new deutschPerson("Thomas"),new deutschPerson("Carl") };
+    int numberOfPerson = sizeof(pArray) / sizeof(pArray[0]);
+    for (size_t i = 0; i < numberOfPerson; i++)
+    {
+        for (size_t j = 0; j  < numberOfPerson; j ++)
+        {
+            if (i != j) {
+                pArray[i]->sayHi(*pArray[j]);
+            }
+        }
+        cout << "\n";
+    }
+
+
+
+}
+
