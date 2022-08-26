e0c16ee1b356d32eb1cf1b03710615e3e19d2a93
Hüseyin Aslan <91592183+HsynAslan@users.noreply.github.com>
2022-08-26 11:05:39 +0300

    Add files via upload

---
+++ shape/shape/shape.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "shape.h"
+int main()
+{
+	using namespace std;
+
+	Shape* sArr[] = {new Rectangle(4.0,5.0),new Rectangle(3.5,2.5),new Rectangle(8.5,4.3), new Circle(3.2), new Circle(3.8) };
+	int numberShape = sizeof(sArr) / sizeof(sArr[0]);
+	for (size_t i = 0; i < numberShape; i++)
+	{
+		sArr[i]->calculateArea();
+		sArr[i]->calculatePerimater();
+		sArr[i]->display();
+	}
+}
+
