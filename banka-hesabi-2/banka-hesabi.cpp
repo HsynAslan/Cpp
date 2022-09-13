a2b0348879334981f7c7b42686e68a14191672cf
Hüseyin Aslan <91592183+HsynAslan@users.noreply.github.com>
2022-09-13 08:32:43 +0300

    Add files via upload

---
+++ banka-hesabi-2/banka-hesabi.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+using namespace std;
+#include "banka-hesabi.h"
+#include <string>
+int main()
+{
+    bankAccount acc1("Huseyin", "Aslan", 3500, "05071234567");
+    cout << acc1;
+    acc1 + 1500;
+    cout << acc1;
+
+}
+
